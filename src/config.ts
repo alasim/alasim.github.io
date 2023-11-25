a99a2f56334e3fc27b11386fa9b99d0272654e51
Hasan Al Asim <hasan@klwebco.com>
2023-11-25 23:59:29 +0600

    title fix

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Hasan Al Asim | Software Engineer and Entrepreneur';
-export const SITE_DESCRIPTION = 'I am a software engineer and entrepreneur. A seasoned professional with over three years of diverse experience in software development.';
+export const SITE_TITLE = 'Hasan Al Asim | Software Developer and Entrepreneur';
+export const SITE_DESCRIPTION = 'I am a Software Developer and Entrepreneur. A seasoned professional with over three years of diverse experience in software development.';
