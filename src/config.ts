f935af644864e3cfa5abe6f8aad1c5b2b4fd5705
Hasan Al Asim <hasan@klwebco.com>
2023-11-26 00:02:51 +0600

    site description added

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Hasan Al Asim | Software Developer and Entrepreneur';
-export const SITE_DESCRIPTION = 'I am a Software Developer and Entrepreneur. A seasoned professional with over three years of diverse experience in software development.';
+export const SITE_DESCRIPTION = 'I make simple and effective software solutions, focusing on minimalism, problem-solving, and efficient functionality.';
